c76c30291b3a3746402d741a9ab829d916769f53
jz-webservices <info@jzwebservices.at>
2026-03-24 07:21:52 +0100

    Version auf 19.0.1.5.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'JZ Advanced Autosearch – Async Product Search',
-    'version': '19.0.1.4.0',
+    'version': '19.0.1.5.0',
     'summary': 'Asynchrone Live-Produktsuche im eCommerce-Shop',
     'author': 'JZWebservices',
     'category': 'Website/eCommerce',
