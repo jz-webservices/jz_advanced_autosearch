017076aafc84bb6ff75b2c9d14a792bf93884f88
jz-webservices <info@jzwebservices.at>
2026-03-26 17:15:26 +0100

    Fix missing template error: move XML to web.assets_frontend_lazy

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'JZ Advanced Autosearch – Async Product Search',
-    'version': '19.0.1.17.0',
+    'version': '19.0.1.18.0',
     'summary': 'Asynchrone Live-Produktsuche im eCommerce-Shop',
     'author': 'JZWebservices',
     'category': 'Website/eCommerce',
@@ -15,8 +15,10 @@
     'assets': {
         'web.assets_frontend': [
             'jz_advanced_autosearch/static/src/css/product_search.css',
-            'jz_advanced_autosearch/static/src/xml/search_widget.xml',
             'jz_advanced_autosearch/static/src/js/product_search.js',
+        ],
+        'web.assets_frontend_lazy': [
+            'jz_advanced_autosearch/static/src/xml/search_widget.xml',
         ],
     },
     'installable': True,
