b258a0b7a6af72da994ba8b0ab5c48e519f8b822
jz-webservices <info@jzwebservices.at>
2026-04-07 19:15:18 +0200

    fix: replace deprecated type='json' with type='jsonrpc' for Odoo 19

--- controllers/search.py
+++ controllers/search.py
@@ -7,7 +7,7 @@ class JzAdvancedSearchProductSearch(http.Controller):
 
     @http.route(
         '/jz_advanced_autosearch/search/products',
-        type='json',
+        type='jsonrpc',
         auth='public',
         methods=['POST'],
         website=True,
